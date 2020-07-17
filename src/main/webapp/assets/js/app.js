46bf8459be3566662b811da26efdf6bc63b2e1a2
billmal <billmal071@gmail.com>
2020-07-17 10:27:11 +0100

    full page delivered

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -1,21 +1,23 @@
 document.getElementById('subscribe-btn').addEventListener('click', checkForInput);
 
-let form1 = document.forms.myForm.email.value;
+let x = document.forms.myForm.email.value;
+var atposition = x.indexOf("@");
+var dotposition = x.lastIndexOf(".");
 let regexEmail = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$';
 
 function checkForInput() {
-    if (form1 == "" || form1 == null) {
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
         //setTimeout(document.getElementById("fail").style.display='block', 1000);
-        document.getElementById("fail").style.display='block';
+        document.getElementById("fail").style.display = 'block';
         document.getElementById('close-f').addEventListener('click', () => {
-                document.getElementById('fail').style.display='none';
+            document.getElementById('fail').style.display = 'none';
         })
     } else {
         //setTimeout(document.getElementById("success").style.display='block', 1000);
-        document.getElementById("success").style.display='block';
+        document.getElementById("success").style.display = 'block';
         document.getElementById('close-s').addEventListener('click', () => {
-                document.getElementById('success').style.display = 'none';
-            })
+            document.getElementById('success').style.display = 'none';
+        })
     }
 }
 
