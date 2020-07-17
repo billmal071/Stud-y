3bd2a7c943ab95e55262cbbf3dc5bae944df8eb0
billmal <billmal071@gmail.com>
2020-07-17 10:11:39 +0100

    full page delivered

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -4,7 +4,7 @@ let form1 = document.forms["myForm"]["email"].value;
 let regexEmail = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$';
 
 function checkForInput() {
-    if (form1 == "") {
+    if (form1 == "" || form1 == null) {
         //setTimeout(document.getElementById("fail").style.display='block', 1000);
         document.getElementById("fail").style.display='block';
         document.getElementById('close-f').addEventListener('click', () => {
