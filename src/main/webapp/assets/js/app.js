99205fd074bad70cf1371c24ab3b08dcd813f790
billmal <billmal071@gmail.com>
2020-07-17 09:11:08 +0100

    images are larger now

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -5,12 +5,14 @@ let regexEmail = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$';
 
 function checkForInput() {
     if (form1 == "") {
-        setTimeout(document.getElementById("fail").style.display='block', 1000);
+        //setTimeout(document.getElementById("fail").style.display='block', 1000);
+        document.getElementById("fail").style.display='block';
         document.getElementById('close-f').addEventListener('click', () => {
                 document.getElementById('fail').style.display='none';
         })
     } else {
-        setTimeout(document.getElementById("success").style.display='block', 1000);
+        //setTimeout(document.getElementById("success").style.display='block', 1000);
+        document.getElementById("success").style.display='block';
         document.getElementById('close-s').addEventListener('click', () => {
                 document.getElementById('success').style.display = 'none';
             })
