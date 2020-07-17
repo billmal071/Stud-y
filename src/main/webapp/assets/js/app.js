385518f63f53489cad90bed207de340767a2c8c9
billmal <billmal071@gmail.com>
2020-07-17 02:29:34 +0100

    responsive and added all images and animations

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -6,13 +6,13 @@ let regexEmail = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$';
 function checkForInput() {
     if (form1 == "") {
         setTimeout(document.getElementById("fail").style.display='block', 1000);
-        document.getElementById('close-f').addEventListener('click', function () {
+        document.getElementById('close-f').addEventListener('click', () => {
                 document.getElementById('fail').style.display='none';
         })
     } else {
         setTimeout(document.getElementById("success").style.display='block', 1000);
-        document.getElementById('close-s').addEventListener('click', function () {
-            document.getElementById('success').style.display='none';
-        })
+        document.getElementById('close-s').addEventListener('click', () => {
+                document.getElementById('success').style.display = 'none';
+            })
     }
 }
